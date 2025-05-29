8294875d9d68bba31b0844cef7976edc3d6ba6bf
Jorge Tomé <jorge.tomejr@outlook.com>
2025-05-29 12:57:07 -0400

    criação das configurações da api

---
+++ src/br/com/conversormoedas/api/ConfiguracoesApi.java
@@ -0,0 +1,14 @@
+package br.com.conversormoedas.api;
+
+public class ConfiguracoesApi {
+    private static String API_KEY = "5109ab11b9fb4000b934e328cced84dd";
+    private static String URL_BASE = "https://api.currencyfreaks.com/v2.0/rates/latest";
+
+    public static String getApiKey(){
+        return API_KEY;
+    }
+
+    public static String getUrlBase(){
+        return getUrlBase();
+    }
+}
